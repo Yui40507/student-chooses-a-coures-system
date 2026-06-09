d7d62fab285cbc51bf5a4d8f859dbb4ea59e430c
tingyu920810 <101988191+tingyu920810@users.noreply.github.com>
2026-06-09 22:48:08 +0800

    Update course.cpp

--- course.cpp
+++ course.cpp
@@ -1,36 +1,43 @@
 #include "course.h"
+#include "student.h"
 
-void Course::addStudent(Student* const student)
-{
+void Course::addStudent(Student* const student) {
     enrolledStudents.push_back(student);
 }
 
-void Course::removeStudent(Student* const student)
-{
-    auto it = find(enrolledStudents.begin(),
-                   enrolledStudents.end(),
-                   student);
+void Course::removeStudent(Student* const student) {
+    auto it = find(
+        enrolledStudents.begin(),
+        enrolledStudents.end(),
+        student
+    );
 
-    if (it != enrolledStudents.end())
-    {
+    if (it != enrolledStudents.end()) {
         enrolledStudents.erase(it);
     }
 }
 
-void Course::printStudents() const
-{
+void Course::printStudents() const {
     cout << "課程 " << title << " 的學生名單：" << endl;
 
-    if (enrolledStudents.empty())
-    {
+    if (enrolledStudents.empty()) {
         cout << "（尚無人修課）" << endl;
         return;
     }
 
-    for (auto s : enrolledStudents)
-    {
-        cout << " - " << s->getId()
-             << ": " << s->getName()
-             << endl;
+    for (auto s : enrolledStudents) {
+        cout << " - "
+            << s->getId()
+            << ": "
+            << s->getName()
+            << endl;
     }
+}
+
+int Course::getCapacity() const {
+    return capacity;
+}
+
+int Course::getEnrolledCount() const {
+    return enrolledStudents.size();
 }
