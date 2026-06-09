580eca327d969838a773c585d0de3ad63db4dd48
tingyu920810 <101988191+tingyu920810@users.noreply.github.com>
2026-06-09 22:48:28 +0800

    Update student.h

--- student.h
+++ student.h
@@ -1,11 +1,11 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include "course.h"
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
+
 using namespace std;
 
 class Course;
@@ -19,24 +19,32 @@ private:
     vector<Course*> enrolledCourses;
 
 public:
-    Student(string id, string name, string password)
-        : id(id), name(name), password(password) {}
+    Student(
+        string id,
+        string name,
+        string password
+    )
+        : id(id),
+        name(name),
+        password(password) {
+    }
 
     string getId() const { return id; }
+
     string getName() const { return name; }
 
-    void setPassword(const string& newPassword)
-    {
+    void setPassword(const string& newPassword) {
         password = newPassword;
     }
 
-    void enroll(Course* course);
-    void drop(Course* course);
-    void printCourses() const;
-
     bool verifyPassword(const string& input) const;
 
-    int getTotalCredits() const;   // ⭐ 學分總計
+    void enroll(Course* course);
+    void drop(Course* course);
+
+    void printCourses() const;
+
+    int getTotalCredits() const;
 };
 
 #endif
